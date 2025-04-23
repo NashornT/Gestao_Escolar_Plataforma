7d54a8da409c55a9c2cc9ca843203ed2f1858b31
Gustavo Barboza <Huedor123@gmail.com>
2025-04-22 21:04:20 -0300

    refactor: enhance data extraction logic and improve key adjustments

--- extract_data.py
+++ extract_data.py
@@ -10,11 +10,9 @@ class ExtractData:
         self.file = None
         self.workbook = None
         self.sheet = None
-        self.studant = None
         self.studant_dict = dict()
         self.disciplines_columns = ['Língua Portuguesa', 'Artes', 'Ciências', 'Matemática', 'Geografia', 'História',
                                     'Cidadania/Ética', 'Inglês']
-        self.other_columns = ['CÓDIGOS E', 'FALTAS', 'RC', '1° Ano de Escolaridade']
 
     def open_file(self):
         """Open the .xls file."""
@@ -24,42 +22,69 @@ class ExtractData:
 
     def get_data(self):
         """Extract data from the .xls file."""
+        self.open_file()
+
+        studant_key = str
         for row_idx in range(self.sheet.nrows):
             studant_data = list()
-            for numb, data in enumerate(self.sheet.row_values(row_idx)):
+            for data in self.sheet.row_values(row_idx):
                 if "Aluno" in str(data):
-                    self.studant = data
-                    self.studant_dict.update({self.studant: {}})
+                    studant_key = data
+                    self.studant_dict.update({studant_key: {}})
                 elif data != "":
                     studant_data.append(data)
 
             if len(studant_data) > 1:
                 key = studant_data[1]
-                if key == "CÓDIGOS E":
-                    key = studant_data[2]
-                    studant_data.remove("CÓDIGOS E")
-                elif key not in self.disciplines_columns:
-                    key = studant_data[0]
 
-                if key in self.disciplines_columns:
-                    for data in studant_data:
-                        if type(data) == str:
-                            studant_data.remove(data)
+                key, studant_key = self.adjustment_keys(key,studant_key, studant_data)
 
-                if key in studant_data:
-                    studant_data.remove(key)
+                if key != studant_key:
+                    self.studant_dict.get(studant_key).update({key: studant_data})
 
-                self.studant_dict.get(self.studant).update({key: studant_data})
+    def adjustment_keys(self, key ,studant_key, studant_data):
+        """Adjust the keys for the dictionary."""
+
+        if studant_key == 'Aluno(a):':
+            del self.studant_dict[studant_key]
+            studant_key = studant_data[0]
+            self.studant_dict.update({studant_key: {}})
+
+        if key == "CÓDIGOS E":
+            key = studant_data[2]
+            studant_data.remove("CÓDIGOS E")
+        elif key not in self.disciplines_columns:
+            key = studant_data[0]
+
+        if key == 'BASE NACIONAL COMUM':
+            key = studant_data[2]
+            studant_data.remove(studant_data[0])
+            studant_data.remove(studant_data[1])
+
+        if key in self.disciplines_columns:
+            for data in studant_data:
+                if type(data) == str:
+                    studant_data.remove(data)
+
+        if key in studant_data:
+            studant_data.remove(key)
+
+        return key, studant_key
 
 
     def manipulate_data(self):
         """Manipulate the extracted data."""
+        self.get_data()
+
         # Convert the dictionary to a DataFrame
         df = pd.DataFrame.from_dict(self.studant_dict, orient='index')
 
         # Remove unwanted columns
-        colunas_para_remover = ['ÁREAS DE', 'RB', 'LEGENDA', 'CONHECIMENTO']
-        df = df.drop(columns=colunas_para_remover)
+        colunas_para_remover = ['ÁREAS DE', 'RB', 'LEGENDA', 'CONHECIMENTO','N','Disciplinas']
+
+        for column in colunas_para_remover:
+            if column in df.columns:
+                df = df.drop(columns=column)
 
         output_folder = self.folder_path + "/outputs"
         if not os.path.exists(output_folder):
@@ -80,6 +105,4 @@ class ExtractData:
             if file.endswith('.xls'):
                 self.file = os.path.join(self.folder_path, file)
                 self.studant_dict = {}
-                self.open_file()
-                self.get_data()
                 self.manipulate_data()
